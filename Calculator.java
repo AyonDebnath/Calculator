5be5e3873ff5053551d0595025fcb2415ad93259
Ayon Chandra Debnath <ayon@Ayons-MacBook-Air.local>
2021-12-20 12:44:34 -0330

    First Layout completed

--- Calculator.java
+++ Calculator.java
@@ -20,9 +20,31 @@ class Calculator
         frame = new JFrame("Calculator");
         Container contentPane = frame.getContentPane();
         
-        JLabel label = new JLabel("I am a label.");
-        contentPane.add(label);
+        JLabel display = new JLabel("null");
+        display.setVerticalAlignment(JLabel.TOP);
 
+        JButton one = new JButton("1");
+        JButton two = new JButton("2");
+        JButton three = new JButton("3");
+        JButton four = new JButton("4");
+        JButton five = new JButton("5");
+        JButton six = new JButton("6");
+        JButton seven = new JButton("7");
+        JButton eight = new JButton("8");
+        JButton nine = new JButton("9");
+
+        contentPane.add(display);
+        contentPane.add(one);
+        contentPane.add(two);
+        contentPane.add(three);
+        contentPane.add(four);
+        contentPane.add(five);
+        contentPane.add(six);
+        contentPane.add(seven);
+        contentPane.add(eight);
+        contentPane.add(nine);
+
+        contentPane.setLayout(new GridLayout(3, 3));
         frame.pack();
         frame.setVisible(true);
     }
